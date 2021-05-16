9f0730b4a5731bc953f3b0f7f124b569c9e44fed
baconj <romans18@gmail.com>
2021-05-15 17:23:59 -0700

    Add files via upload
    Simple Java program that rewrites whatever is input by the user.

---
+++ UserInput/UserInput/src/userInput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String args[]){
+        Scanner jaime = new Scanner(System.in);
+        System.out.println(jaime.nextLine());
+    }
+}
