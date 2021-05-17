ce8a34d38d7aa7182f8bdf2cce4a7ae79e532270
baconj <romans18@gmail.com>
2021-05-17 10:54:47 -0700

    Update apples.java

--- HelloWorld/apples.java
+++ HelloWorld/apples.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class apples {
     public static void main(String args[]){
-        Scanner jaime = new Scanner(System.in);
-        System.out.println(jaime.nextLine());
+        System.out.println("Hello World!")
     }
 }
