db28e5b4f0f6f15975299094c9d4997d7fa733c6
baconj <romans18@gmail.com>
2021-05-17 10:58:00 -0700

    Update testDB.java

--- Java|SQLite/testDB/testDB.java
+++ Java|SQLite/testDB/testDB.java
@@ -32,29 +32,9 @@ public class testDB {
         }
     }
     /**
-     * Class to create a new database
-     */
-   /** public static void createNewDatabase(String fileName) {
-        String url = "jdbc:sqlite:c:/sqlite" + fileName;
-
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-            }
-        }
-    }
-    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         connect();
-    //    createNewDatabase("thisisnew.db");
     }
 }
