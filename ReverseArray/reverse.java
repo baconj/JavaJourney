5504b199d61ddab89135201ca00542244356c415
baconj <romans18@gmail.com>
2021-05-16 19:04:43 -0700

    Update reverse.java
    Simple Function to take an array of numbers and reverse it.

--- ReverseArray/reverse.java
+++ ReverseArray/reverse.java
@@ -1,1 +1,15 @@
+public class Reverse {
+    public static void main(String[] args) {
 
+        int[] array = new int[] {2,4,5,6,7,8,9};
+                System.out.println("Original Array:");
+                for(int i = 0; i < array.length; i++)
+                    System.out.print(array[i] + " ");
+
+                System.out.println();
+
+                System.out.println("Array in reverse");
+                for(int i=array.length-1;i>=0;i--)
+                    System.out.print(array[i] + " ");
+            }
+    }
