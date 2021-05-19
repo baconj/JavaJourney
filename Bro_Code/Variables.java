927f492c466ea23de8e3ab7db83c5554ae39a9e8
baconj <romans18@gmail.com>
2021-05-18 20:16:22 -0700

    Add files via upload

---
+++ Bro_Code/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args){
+
+     //  int x; //declaration
+
+     //  x = 123; //assignment
+
+       int x = 123; //initialization
+        double a = 3.14; //no f
+        float z = 3.14f; //followed with f
+        boolean b = true; //True or False
+        char symbol = '@'; //single char
+        String name = "Jaime"; //String is a reference data type, begins with capital
+        long y = 12310298310938018L; //must be followed with an L
+
+       System.out.println("My number is: " + x);
+
+    }
+}
